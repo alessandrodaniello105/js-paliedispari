d25cc07b4b26c6a0463c6422514039b57633f75f
alessandrodaniello105 <kanymsevement@gmail.com>
2023-09-15 16:23:01 +0200

    [add]: isPali function

--- js/script.js
+++ js/script.js
@@ -3,9 +3,18 @@
 const nameUser = prompt('Inserisci il tuo nome');
 
 inverti(nameUser);
-console.log(inverti(nameUser));
+// console.log(inverti(nameUser));
 
+
+// 2.
 function inverti(text) {
   return text.split('').reverse().join('');
 };
 
+isPali();
+
+// 3.
+function isPali(x) {
+  x = (nameUser == inverti(nameUser) ? true : false);
+  console.log(x);
+}
